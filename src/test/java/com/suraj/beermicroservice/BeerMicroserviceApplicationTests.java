2ea171de6ce53d57010b9d40a8a6b1d1d6dd84f5
Suraj Sharma <surajsharma286@gmail.com>
2021-03-22 22:58:09 +0530

    Initial Project

---
+++ src/test/java/com/suraj/beermicroservice/BeerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.beermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
